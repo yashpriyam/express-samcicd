d2d56fb4a9a33d4a9a8a741dc6ca84a430e01f73
Yash priyam <yashpriyam74@gmail.com>
2022-12-16 16:55:18 +0530

    remove console

--- app/app.js
+++ app/app.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const serverless = require('serverless-http')
+const express = require("express");
+const serverless = require("serverless-http");
 // const Todo = require('./model/todo')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const todoSchema = new mongoose.Schema({
-    title: {type: String, required:true},
-    description: {type: String, required:true},
-    completed: {type: Boolean, default: false},
-})
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
 
 // mongodb model and connect to database
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
 const MONGODB_URI =
   "mongodb+srv://yash:yash12345@cluster0.firlmgw.mongodb.net/?retryWrites=true&w=majority";
-
 
 async function connectDB() {
   const client = await mongoose.connect(`${MONGODB_URI}`, {
@@ -25,7 +24,7 @@ async function connectDB() {
 }
 connectDB();
 
-const app = express()
+const app = express();
 
 // middlewares
 // app.options('*', cors({
@@ -34,78 +33,78 @@ const app = express()
 //         'Access-Control-Allow-Origin', '*'
 //     ]
 // }))
-app.use(cors({
-    origin: "*"
-}))
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // todo routes
 
-app.get('/hell', (req, res) => res.send('Hello World'))
+app.get("/hell", (req, res) => res.send("Hello World"));
 
 app.post("/create", async (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    try {
-      const todoData = await Todo.create({ title, description });
-      return res.status(200).send(JSON.stringify(todoData));
-    } catch (error) {
-      console.log(error.message);
-      res.status(404).send({ error: error.message });
-    }
-  });
+  const title = req.body.title;
+  const description = req.body.description;
+  try {
+    const todoData = await Todo.create({ title, description });
+    return res.status(200).send(JSON.stringify(todoData));
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).send({ error: error.message });
+  }
+});
 
 app.post("/update/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-      const todoData = await Todo.findOne({ _id: id });
-      const todoCompleted = todoData.completed ? false : true;
-      const updateTodo = await Todo.updateOne(
-        { _id: id },
-        {
-          $set: {
-            completed: todoCompleted,
-          },
-        }
-      );
-      return res.status(200).send(JSON.stringify(updateTodo));
-    } catch (error) {
-      console.log(error.message);
-      res.status(404).send({ error: error.message });
-    }
-  });
-  app.post("/update/todo/", async (req,res) => {
-    const id = req.body.id;
-    const updateTodo = {
-        title: req.body.title,
-        description: req.body.description
-    }
-    try {
-      const todoData = await Todo.findByIdAndUpdate(id, updateTodo, {
-        new: true
-      } );
-      return res.status(200).send(JSON.stringify(todoData));
-    } catch (error) {
-      console.log(error.message);
-      res.status(404).send({ error: error.message });
-    }
-})
+  const id = req.params.id;
+  try {
+    const todoData = await Todo.findOne({ _id: id });
+    const todoCompleted = todoData.completed ? false : true;
+    const updateTodo = await Todo.updateOne(
+      { _id: id },
+      {
+        $set: {
+          completed: todoCompleted,
+        },
+      }
+    );
+    return res.status(200).send(JSON.stringify(updateTodo));
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).send({ error: error.message });
+  }
+});
+app.post("/update/todo/", async (req, res) => {
+  const id = req.body.id;
+  const updateTodo = {
+    title: req.body.title,
+    description: req.body.description,
+  };
+  try {
+    const todoData = await Todo.findByIdAndUpdate(id, updateTodo, {
+      new: true,
+    });
+    return res.status(200).send(JSON.stringify(todoData));
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).send({ error: error.message });
+  }
+});
 
-app.get('/todos',  async(req, res) => {
-    const todos = await Todo.find({});
-    res.json({success:true, todos})
-})
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find({});
+  res.json({ success: true, todos });
+});
 
 app.delete("/delete", async (req, res) => {
-    const id = req.body.todoId;
-    try {
-      const todoData = await Todo.deleteOne({ _id: id });
-      return res.status(200).send(JSON.stringify(todoData));
-    } catch (error) {
-      console.log(error.message);
-      res.status(404).send({ error: error.message });
-    }   
-  });
+  const id = req.body.todoId;
+  try {
+    const todoData = await Todo.deleteOne({ _id: id });
+    return res.status(200).send(JSON.stringify(todoData));
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).send({ error: error.message });
+  }
+});
 
-// app.listen(3000, () => console.log('Running on port 3000'))
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
