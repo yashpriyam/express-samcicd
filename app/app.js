07aed8bb660e191a8ebcf86f6759e850b538fabd
yashpriyam <yashpriyam74@gmail.com>
2022-12-16 13:55:43 +0530

    add update route

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const todoSchema = new mongoose.Schema({
     title: {type: String, required:true},
+    description: {type: String, required:true},
     completed: {type: Boolean, default: false},
 })
 
@@ -41,16 +42,19 @@ app.use(cors({
 // todo routes
 
 app.get('/hell', (req, res) => res.send('Hello World'))
+
 app.post("/create", async (req, res) => {
-    const todo = req.body.title;
+    const title = req.body.title;
+    const description = req.body.description;
     try {
-      const todoData = await Todo.create({ title: todo });
+      const todoData = await Todo.create({ title, description });
       return res.status(200).send(JSON.stringify(todoData));
     } catch (error) {
       console.log(error.message);
       res.status(404).send({ error: error.message });
     }
   });
+
 app.post("/update/:id", async (req, res) => {
     const id = req.params.id;
     try {
@@ -70,10 +74,28 @@ app.post("/update/:id", async (req, res) => {
       res.status(404).send({ error: error.message });
     }
   });
+  app.post("/update/todo/", async (req,res) => {
+    const id = req.body.id;
+    const updateTodo = {
+        title: req.body.title,
+        description: req.body.description
+    }
+    try {
+      const todoData = await Todo.findByIdAndUpdate(id, updateTodo, {
+        new: true
+      } );
+      return res.status(200).send(JSON.stringify(todoData));
+    } catch (error) {
+      console.log(error.message);
+      res.status(404).send({ error: error.message });
+    }
+})
+
 app.get('/todos',  async(req, res) => {
     const todos = await Todo.find({});
     res.json({success:true, todos})
 })
+
 app.delete("/delete", async (req, res) => {
     const id = req.body.todoId;
     try {
